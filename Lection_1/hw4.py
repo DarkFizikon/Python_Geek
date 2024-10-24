4f15427e78b887f60d628975bd91ff4356171341
alex <aleks15.12@mail.ru>
2024-10-24 16:45:33 +0300

    Homework task 4

---
+++ Lection_1/hw4.py
@@ -0,0 +1,5 @@
+lenght1 = int(input('The length of the first catheter: '))
+lenght2 = int(input('The length of the second catheter: '))
+
+square = (lenght1*lenght2)/2
+print(f"Еhe area of a right triangle is {square}")
