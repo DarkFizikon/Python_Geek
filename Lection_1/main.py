0e19f4d1e7f3ab6c1d4d9c635d1d0aeb6824803b
alex <aleks15.12@mail.ru>
2024-10-24 15:46:27 +0300

    Simples lection and seminar

---
+++ Lection_1/main.py
@@ -0,0 +1,21 @@
+# a = 5
+# b= 5.89
+# c ='Hello'
+
+# print(f"{a} - {b} - {c}")
+
+# print("{} - {} - {}".format(a,b,c))
+
+print('Entry first num:')
+a = float(input())
+
+b = float(input('Entry second num: '))
+print(a, '+', b, '=', a+b)
+print(round(a*b, 3))
+
+""" c= 5.89
+print(c)
+print(type(c))
+n=int(c)
+print(n)
+print(type(n)) """
