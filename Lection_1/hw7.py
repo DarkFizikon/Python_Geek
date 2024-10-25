c263d00fe2b8747dcb671a02b4b1f3d21ff41960
alex <aleks15.12@mail.ru>
2024-10-25 11:10:21 +0300

    Homework task 7 modifide

--- Lection_1/hw7.py
+++ Lection_1/hw7.py
@@ -1,15 +1,16 @@
 n = 8
 i = 1
 sum_tusk = 0
-sum_call = 0
+go_to_store = False
 print("The working day has start")
 while i <= n:
     task = int(input(f"How much tusks solution Maks in {i} hours: "))
     sum_tusk += task
     call = int(input("Wife is calling. Pick up the phone? (0 - no; 1 - yes): "))
-    sum_call += call
-    i += 1
+    if call == 1:
+        go_to_store = True
+    i +=1
 
 print(f"The working day is over. Total tasks completed = {sum_tusk}")
-if sum_call > 0:
+if go_to_store:
     print("He need to go to the store")
