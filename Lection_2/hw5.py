108454d0b21c559b91a8aca6bb57c9a73cab92b0
alex <aleks15.12@mail.ru>
2024-10-29 17:36:59 +0300

    5 tasks copy-past

---
+++ Lection_2/hw5.py
@@ -0,0 +1,20 @@
+orders_count = int(input("Enter the number of orders: "))
+
+database = dict()
+
+for i_order in range(orders_count):
+    customer, pizza_name, count = input('{} order: '.format(i_order + 1)).split()
+    count = int(count)
+
+    if customer not in database:
+        database[customer] = {pizza_name: count}
+    else:
+        if pizza_name in database[customer]:
+            database[customer][pizza_name] += count
+        else:
+            database[customer][pizza_name] = count
+
+for customer in sorted(database.keys()):
+    print('{}:'.format(customer))
+    for pizza_name in sorted(database[customer].keys()):
+        print('      {}: {}'.format(pizza_name, database[customer][pizza_name]))
